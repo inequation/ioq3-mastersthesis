653028dc7574e11a4fbfed0974f083fbfbcca831
Leszek Godlewski <github@inequation.org>
2014-08-22 20:34:58 +0200

    added functionality for queueing object mutations for the end of the frame

--- code/game/DoubleBuffering.cpp
+++ code/game/DoubleBuffering.cpp
@@ -1,7 +1,11 @@
 #include "g_local.h"
-#include "EntPtr.h"
+#include "DoubleBuffering.h"
+
+#include <tbb/concurrent_queue.h>
 
 tbb::enumerable_thread_specific<gentity_t *> EntityContext::Context;
+
+// ===========================================================================
 
 static const gentity_t *GetROPointer(const gentity_t *ent)
 {
@@ -28,6 +32,8 @@ static gentity_t *GetProperPointer(const gentity_t *ent)
 			?              GetRWPointer(ent)	// writing to ourselves is OK
 			: (gentity_t *)GetROPointer(ent);	// writing to others is not OK
 }
+
+// ===========================================================================
 
 EntPtr::EntPtr()
 	: Ptr(nullptr)
@@ -82,4 +88,144 @@ EntPtr& EntPtr::operator++()
 EntPtr EntPtr::operator++(int)
 {
 	return EntPtr(Ptr++);
+}
+
+gentity_t *EntPtr::GetRWPointer() const
+{
+	return ::GetRWPointer(Ptr);
+}
+
+const gentity_t *EntPtr::GetROPointer() const
+{
+	return ::GetROPointer(Ptr);
+}
+
+bool EntPtr::operator==(const gentity_t *entity) const
+{
+	return GetROPointer() == ::GetROPointer(entity);
+}
+
+bool EntPtr::operator!=(const gentity_t *entity) const
+{
+	return GetROPointer() != ::GetROPointer(entity);
+}
+
+// ===========================================================================
+
+extern gclient_t *g_clients;
+
+namespace Mutation
+{
+	struct Mutation
+	{
+		enum
+		{
+			ENTITY,
+			CLIENT
+		}			Type;
+		void		*Object;
+		size_t		Offset;
+		size_t		Length;
+		void		*Data;
+	};
+	static tbb::concurrent_bounded_queue<Mutation> gMutationQueue;
+
+	void SetBytes(EntPtr Entity, size_t Offset, size_t Length, void *Data)
+	{
+		auto Context = EntityContext::GetEntity();
+
+		if (Context == nullptr || Context == Entity)
+		{
+			// fast path – we have RW access, just copy it over
+			memcpy((char *)Entity.GetRWPointer() + Offset, Data, Length);
+			return;
+		}
+
+		// slow path – queue for commit at end of frame
+		Mutation m;
+		m.Type = Mutation::ENTITY;
+		m.Object = Entity;
+		m.Offset = Offset;
+		m.Length = Length;
+		m.Data = malloc(Length);
+		memcpy(m.Data, Data, Length);
+		gMutationQueue.push(m);
+	}
+
+	void SetBytes(struct gclient_s *Client, size_t Offset, size_t Length, void *Data)
+	{
+		auto Context = EntityContext::GetEntity();
+
+		if (Context == nullptr || Context == &g_entities[Client - g_clients])
+		{
+			// fast path – we have RW access, just copy it over
+			memcpy((char *)Client + Offset, Data, Length);
+			return;
+		}
+
+		// slow path – queue for commit at end of frame
+		Mutation m;
+		m.Type = Mutation::CLIENT;
+		m.Object = Client;
+		m.Offset = Offset;
+		m.Length = Length;
+		m.Data = malloc(Length);
+		memcpy(m.Data, Data, Length);
+		gMutationQueue.push(m);
+	}
+
+	void ClearBytes(EntPtr Entity, size_t Offset, size_t Length)
+	{
+		auto Context = EntityContext::GetEntity();
+
+		if (Context == nullptr || Context == Entity)
+		{
+			// fast path – we have RW access, just copy it over
+			memset((char *)Entity.GetRWPointer() + Offset, 0, Length);
+			return;
+		}
+
+		// slow path – queue for commit at end of frame
+		Mutation m;
+		m.Type = Mutation::ENTITY;
+		m.Object = Entity;
+		m.Offset = Offset;
+		m.Length = Length;
+		m.Data = malloc(Length);
+		memset(m.Data, 0, Length);
+		gMutationQueue.push(m);
+	}
+
+	void ClearBytes(struct gclient_s *Client, size_t Offset, size_t Length)
+	{
+		auto Context = EntityContext::GetEntity();
+
+		if (Context == nullptr || Context == &g_entities[Client - g_clients])
+		{
+			// fast path – we have RW access, just copy it over
+			memset((char *)Client + Offset, 0, Length);
+			return;
+		}
+
+		// slow path – queue for commit at end of frame
+		Mutation m;
+		m.Type = Mutation::CLIENT;
+		m.Object = Client;
+		m.Offset = Offset;
+		m.Length = Length;
+		m.Data = malloc(Length);
+		memset(m.Data, 0, Length);
+		gMutationQueue.push(m);
+	}
+
+	void CommitMutations()
+	{
+		Mutation m;
+		while (gMutationQueue.try_pop(m))
+		{
+			void *Ptr = (char *)(m.Type == Mutation::ENTITY ? ::GetRWPointer((gentity_t *)m.Object) : m.Object) + m.Offset;
+			memcpy(Ptr, m.Data, m.Length);
+			free(m.Data);
+		}
+	}
 }
--- code/game/DoubleBuffering.h
+++ code/game/DoubleBuffering.h
@@ -19,19 +19,39 @@ public:
 	EntPtr operator++(int);
 
 	bool operator==(const EntPtr& Other) const
-	{return Ptr == Other.Ptr;}
+	{return GetROPointer() == Other.GetROPointer();}
 	bool operator!=(const EntPtr& Other) const
-	{return Ptr != Other.Ptr;}
-	bool operator==(const gentity_t *entity) const
-	{return Ptr == entity;}
-	bool operator!=(const gentity_t *entity) const
-	{return Ptr != entity;}
+	{return GetROPointer() != Other.GetROPointer();}
+	bool operator==(const gentity_t *entity) const;
+	bool operator!=(const gentity_t *entity) const;
+
+	// Explicitly fetches pointers of given type. Use with care!
+	gentity_t *GetRWPointer() const;
+	const gentity_t *GetROPointer() const;
 
 private:
 	gentity_t	*Ptr;
 
 	friend class ScopedEntityContext;
 };
+
+// ===========================================================================
+
+namespace Mutation
+{
+	void SetBytes(EntPtr Entity, size_t Offset, size_t Length, void *Data);
+	void SetBytes(struct gclient_s *Client, size_t Offset, size_t Length, void *Data);
+	void ClearBytes(EntPtr Entity, size_t Offset, size_t Length);
+	void ClearBytes(struct gclient_s *Client, size_t Offset, size_t Length);
+	void CommitMutations();
+
+	template <class T>
+	void Set(EntPtr Entity, size_t Offset, T Value)
+	{SetBytes(Entity, Offset, sizeof(T), &Value);}
+	template <class T>
+	void Set(struct gclient_s *Client, size_t Offset, T Value)
+	{SetBytes(Client, Offset, sizeof(T), &Value);}
+}
 
 // ===========================================================================
 
