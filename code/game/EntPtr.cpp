52f6464408dd967b147512a42f2579530d106194
Leszek Godlewski <github@inequation.org>
2014-08-22 17:38:16 +0200

    added code to automatically use the correct pointer depending on entity context

--- code/game/EntPtr.cpp
+++ code/game/EntPtr.cpp
@@ -1,6 +1,34 @@
 #include "g_local.h"
 #include "EntPtr.h"
 
+tbb::enumerable_thread_specific<gentity_t *> EntityContext::Context;
+
+static const gentity_t *GetROPointer(const gentity_t *ent)
+{
+	if (ent - g_entities_old >= 0 && ent - g_entities_old < MAX_GENTITIES)
+		return ent;
+	return &g_entities_old[ent - g_entities];
+}
+
+static gentity_t *GetRWPointer(const gentity_t *ent)
+{
+	if (ent - g_entities >= 0 && ent - g_entities < MAX_GENTITIES)
+		return (gentity_t *)ent;
+	return &g_entities[ent - g_entities_old];
+}
+
+static gentity_t *GetProperPointer(const gentity_t *ent)
+{
+	if (g_entities_old == g_entities)
+		return (gentity_t *)ent;	// on first frame never modify the pointer
+	if (ent == nullptr)
+		return nullptr;
+	auto Context = EntityContext::GetEntity();
+	return (Context == ent || Context == nullptr)
+			?              GetRWPointer(ent)	// writing to ourselves is OK
+			: (gentity_t *)GetROPointer(ent);	// writing to others is not OK
+}
+
 EntPtr::EntPtr()
 	: Ptr(nullptr)
 {
@@ -8,13 +36,13 @@ EntPtr::EntPtr()
 }
 
 EntPtr::EntPtr(gentity_t *entity)
-	: Ptr(entity)
+	: Ptr(GetProperPointer(entity))
 {
 
 }
 
 EntPtr::EntPtr(const EntPtr& Other)
-	: Ptr(Other.Ptr)
+	: Ptr(GetProperPointer(Other.Ptr))
 {
 
 }
@@ -26,7 +54,7 @@ EntPtr::~EntPtr()
 
 EntPtr& EntPtr::operator=(gentity_t *entity)
 {
-	Ptr = entity;
+	Ptr = GetProperPointer(entity);
 	return *this;
 }
 
--- code/game/EntPtr.h
+++ code/game/EntPtr.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <tbb/enumerable_thread_specific.h>
+
 class EntPtr
 {
 public:
@@ -27,4 +29,30 @@ public:
 
 private:
 	gentity_t	*Ptr;
+
+	friend class ScopedEntityContext;
+};
+
+// ===========================================================================
+
+struct EntityContext
+{
+	static gentity_t *& GetEntity()
+	{return Context.local();}
+
+private:
+	static tbb::enumerable_thread_specific<gentity_t *> Context;
+};
+
+struct ScopedEntityContext
+{
+	ScopedEntityContext(const EntPtr& Current)
+		: PrevPtr(EntityContext::GetEntity())
+	{EntityContext::GetEntity() = Current.Ptr;}
+
+	~ScopedEntityContext()
+	{EntityContext::GetEntity() = PrevPtr;}
+
+private:
+	gentity_t	*PrevPtr;
 };
